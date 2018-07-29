a381d223ded10164f0ab8a34f95d026885dc59ee
way2github <mr.ayan.mitra@gmail.com>
2018-07-29 12:09:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const favicon = require('serve-favicon');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(favicon('./favicon.ico'));
+
+
+//middleware
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log', log+'\n');
+    next();
+})
+
+/*
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+*/
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+
+
+app.get('/', (req, res) => {
+    //res.send("<h1>Hello Express!</h1>");
+    res.render('home.hbs',{
+        pageTitle: 'My Home Page',
+        welcomeMessage: 'Welcome to My Page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send("<h1>Hello Express!</h1>");
+    res.render('about.hbs',{
+        pageTitle:'About My Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    //res.send("<h1>Hello Express!</h1>");
+    res.send({
+        title: 'Bad page',
+        status: 403,
+        contact: 'Administrator'
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('server is up and running');
+});
